4dc473f9683f4dad7f5217e446e976594e761664
Gehan Fernando <f.gehan@hotmail.com>
2023-05-27 17:18:28 +0200

    Added Effective Error Handling in Concurrent Task Execution

---
+++ TaskErrors/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S112:General exceptions should never be thrown")]
---
+++ TaskErrors/Program.cs
@@ -0,0 +1,37 @@
+﻿// Developer ::> Gehan Fernando
+
+await MainMethodAsync();
+
+static async Task MethodOneAsync()
+{
+    await Task.Delay(TimeSpan.FromSeconds(1));
+    throw new Exception("Custom Error Method One");
+}
+
+static async Task MethodTwoAsync()
+{
+    await Task.Delay(TimeSpan.FromSeconds(2));
+    throw new Exception("Custom Error Method Two");
+}
+
+static async Task MainMethodAsync()
+{
+    var taskOne = MethodOneAsync();
+    var taskTwo = MethodTwoAsync();
+
+    var tasks = new Task[] { taskOne, taskTwo };
+
+    try
+    {
+        await Task.WhenAll(taskOne, taskTwo);
+    }
+    catch
+    {
+        var exceptions = tasks.SelectMany(task => task.Exception?.InnerExceptions ?? Enumerable.Empty<Exception>());
+
+        foreach (var exception in exceptions)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
